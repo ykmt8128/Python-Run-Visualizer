9d100a73acc9e489ee7781d5feff6da94af6f36b
ykmt8128 <j20397ym@edu.tuis.ac.jp>
2022-01-27 17:32:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,39 +17,41 @@ from pygments import highlight
 from glotio import Runner
 
 
+# 実行パスからの相対パスをもとに絶対パスを返す
 def get_path(filename):
     current_path = os.path.dirname(os.path.abspath(sys.argv[0]))
     return os.path.join(current_path, filename)
 
-class MyFormatter(ImageFormatter):
-    def format(self, tokensource, outfile):
-        with open(outfile, mode="wb+") as f:
-            for ttype, value in tokensource:
-                print("ttype", ttype)
-                print("value", value)
-
+# トレースログをもとにハイライトと出力をコマンド化したリスト形式に変換
 def parse_trace(trace_stdout, filename):
     trace_step = []
     for line in trace_stdout.split("\n"):
+        # 実行している行のログ
         if line.startswith(f"{filename}("):
-            line_num = int(line[len(f"{filename}("):line.index(")")])  # TODO: 正規表現にする
-            code_line = line.split(" ", maxsplit=1)[1]
+            line_num = int(line[len(f"{filename}("):line.index(")")])
             trace_step.append(["highlight", line_num])
+        # 概要ログ
         elif line.startswith(f" --- "):
             pass
+        # 出力ログ
         else:
             trace_step.append(["stdout", line])
     
     return trace_step
 
-
+# GIFを生成し、成功=0, 失敗=-1を返す
 def make_gif(source_filepath, is_online=False):
     filename = os.path.basename(source_filepath)
 
-    #try:
-    with open(source_filepath, encoding="utf-8") as f:
-        sourcecode = f.read()
+    # Pythonのソースコードを読み込み
+    try:
+        with open(source_filepath, encoding="utf-8") as f:
+            sourcecode = f.read()
+    except Exception as e:
+        print(e)
+        return -1
 
+    # オンラインインタプリタを使用
     if is_online:
         cmd = f"python3 -m trace --trace {filename}"
         glot = Runner("c42f1611-017f-4990-842b-a6b08b035d95")
@@ -57,6 +59,7 @@ def make_gif(source_filepath, is_online=False):
         glot.set_code(sourcecode, filename=filename)
         glot.set_command(cmd)
         result = glot.run()["stdout"]
+    # ローカル環境のPythonを使用
     else:
         cmd = f"python3 -m trace --trace {source_filepath}"
         result = subprocess.run(cmd, shell=True, text=True, stdout=subprocess.PIPE).stdout
@@ -68,23 +71,22 @@ def make_gif(source_filepath, is_online=False):
     shutil.rmtree(get_path("tmp"))
     os.mkdir("tmp")
 
+    # GIFの各フレームの画像を生成
     image_list = []
     image_num = 0
     stdout = ""
     for step in trace_step:
         if step[0] == "highlight":
-            # define formatter
+            # フォーマッタを定義
             formatter = ImageFormatter(linenos=False, hl_lines=(step[1],), hl_color="#bbbbbb")
-            #formatter.fonts = FontManager("msgothic.ttc", 14)
-            # tmp
             tmp_image_path = get_path(os.path.join("tmp", f"highlighted{image_num}.png"))
             image_num += 1
-            highlight(sourcecode, PythonLexer(), formatter, tmp_image_path)  # ハイライト表示を追加して保存
-            
+            # ハイライト表示を追加して保存
+            highlight(sourcecode, PythonLexer(), formatter, tmp_image_path)
+            # フレームの画像を加工し、プログラムの出力を記述
             frame_image = PIL.Image.open(tmp_image_path)
             resized_frame_image = Image.new(frame_image.mode, (frame_image.width + 200, frame_image.height), "white")
             resized_frame_image.paste(frame_image, (0, 0))
-            #frame_image.resize((frame_image.width + 300, frame_image.height))
             draw = ImageDraw.Draw(resized_frame_image)
             font = ImageFont.truetype("meiryo.ttc", 14)
             draw.text((frame_image.width + 8, 8), stdout, (0, 0, 0), font=font)
@@ -92,11 +94,10 @@ def make_gif(source_filepath, is_online=False):
 
             image_list.append(resized_frame_image)
 
-            #image_elem.update("highlighted.tif")
-            #time.sleep(2)
-            #asyncio.sleep(1)
         elif step[0] == "stdout":
             stdout += step[1] + "\n"
+    
+    # 各フレームの画像を結合してGIFに変換
     try:
         image_list[0].save(get_path("highlighted.gif"),
             save_all=True, append_images=image_list[1:], duration=1000, loop=0)
@@ -107,9 +108,9 @@ def make_gif(source_filepath, is_online=False):
     return 0
 
 def main():
+    # GUIの設定
     sg.theme("DarkBlue14")
     TITLE = "Python Run Visualizer"
-    #default_image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "default.tif")
     image_elem = sg.Image(None, key="-RESULT_GIF-")
 
     layout = [
@@ -121,9 +122,6 @@ def main():
 
     window = sg.Window(TITLE, layout, size=(900, 600))
 
-    filepath = None
-    filename = None
-
     gif_set_flag = False
     num = 0
     while True:  # Event Loop
@@ -131,7 +129,8 @@ def main():
         if event in (sg.WIN_CLOSED, '終了'):
             break
         elif event == "開く":
-            source_filepath = sg.popup_get_file("Pythonファイルを選択")  # TODO: キャンセル時や空欄のエラーハンドリング
+            source_filepath = sg.popup_get_file("Pythonファイルを選択")
+            # キャンセル時
             if source_filepath is None:
                 continue
             elif not os.path.isfile(source_filepath):
@@ -172,6 +171,7 @@ def main():
 
             shutil.copy(get_path("highlighted.gif"), save_file_path)
 
+        # フレームの更新
         if event == '-UPDATE_GIF-' and gif_set_flag:
             tmp_path = get_path(os.path.join("tmp", f"highlighted{num}.png"))
             if os.path.isfile(tmp_path):
@@ -180,7 +180,6 @@ def main():
             else:
                 gif_set_flag = False
                 num = 0
-
 
     window.close()
 
